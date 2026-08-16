dd2fa732067a9b60a8546f11245f38e90cdff451
abcdlsj <lisongjianshuai@gmail.com>
2026-08-16 17:23:48 +0800

    fix: bump protocol version for preemptive spool replay

--- rpc.go
+++ rpc.go
@@ -18,7 +18,7 @@ const (
 // ProtocolVersion identifies the RPC protocol spoken by the server. Clients
 // use it to detect an outdated server process during upgrades instead of
 // failing on unknown methods.
-const ProtocolVersion = "0.3.7"
+const ProtocolVersion = "0.3.8"
 
 const (
 	rpcMethodCreate        = "create"
